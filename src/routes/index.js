991026f5fdfe9ffea498166054377c6d089c3355
emerlly <ts-emerlly_@hotmail.com>
2023-09-28 23:21:55 -0300

    mudando a pagina de stilo

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+import{ createNativeStackNavigator} from '@react-navigation/native-stack'
+import SigIn from '../pages/Cadastro';
+import Welcome from '../pages/welcome';
+const Stack = createNativeStackNavigator();
+
+export default function Rotes(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen
+            name = "Welcome"
+            component={Welcome}
+            options={ {headerShown: false} }
+            />
+
+            <Stack.Screen
+                name = "SigIn"
+                component={SigIn}
+                options={ {headerShown: false} }
+            />
+        </Stack.Navigator>
+    )
+}
